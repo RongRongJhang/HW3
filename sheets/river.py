cd64d19fa015342ce777f7c31bf53982fe555d8d
RongRongJhang <dandelionron@gmail.com>
2025-05-05 01:51:42 +0800

    first commit

---
+++ sheets/river.py
@@ -0,0 +1,25 @@
+'''
+Title: River flows in you
+Key: A大調(ABC#DEF#G#A)
+Piano Sheet Source: https://douq.pixnet.net/blog/post/3862441
+'''
+
+tempo = 65
+
+# 右手主旋律
+right_score = [
+
+]
+
+right_beat = [
+
+]
+
+# 左手伴奏
+left_score = [
+
+]
+
+left_beat = [
+
+]
